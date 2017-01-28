2a8490c0364676fb18d9dad6595899111d8b3f85
l7ucian <lucian.andercou@gmail.com>
2017-01-28 15:58:59 +0200

    Update connect_to_vpn.py

--- connect_to_vpn.py
+++ connect_to_vpn.py
@@ -1,9 +1,8 @@
 from  Click_around_base import click_around
 import time, win32gui
 
-conti_password = "Conti2Pass"
-#login_window = "Cisco AnyConnect | Auburn Hills"
-login_window = "Cisco AnyConnect | Frankfurt"
+password = ""              # add your password here
+login_window = ""          # add your window name here
 
 w = win32gui
 x = click_around()
@@ -15,5 +14,5 @@ while login_window != w.GetWindowText(w.GetForegroundWindow()):
     x.left_click(1350, 860)  # click to check if connection possible
     time.sleep(1)
 x.left_click(1350,910)   #good passwird to connect to vpn coordinates
-x.write_mambo_jambo(conti_password)
+x.write_mambo_jambo(password)
 
