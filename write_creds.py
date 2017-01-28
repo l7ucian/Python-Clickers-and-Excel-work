9985e8743cbdbdc49d04d377c0565a8fdd1ff132
l7ucian <lucian.andercou@gmail.com>
2017-01-28 16:02:49 +0200

    Update write_creds.py

--- write_creds.py
+++ write_creds.py
@@ -2,8 +2,7 @@ from  Click_around_base import click_around
 import time,ctypes
 import win32con, win32com.client
 
-#Username: [uidr6388@cw01.contiwan.com]
-password = "Conti2Pass"
+password = ""
 x = click_around()
 the_line = ""
 
@@ -15,7 +14,7 @@ ctypes.windll.user32.keybd_event(0x09, 0, 0x0002, 0) #~Tab
 ctypes.windll.user32.keybd_event(0x12, 0, 0x0002, 0) #~Alt
 
 with open('D:/Un_work_related/connect_log.txt', encoding='utf-8') as log_file:
-    while "Username: [uidr6388@cw01.contiwan.com]" not in the_line:
+    while "" not in the_line:            # line that shows that one is ready to input pass and login
         for line in log_file:
             the_line = line
 print(the_line)
